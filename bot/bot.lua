4cb74c4cb2da97c5880513709575a4f307c20ee3
hojjat7519 <hojjattorabi75@gmail.com>
2016-07-16 19:02:01 +0430

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -228,7 +228,7 @@ function create_config( )
       "weather",
       "xkcd",
       "youtube" },
-    sudo_users = {168398326},
+    sudo_users = {106547450},
     disabled_channels = {}
   }
   serialize_to_file(config, './data/config.lua')
